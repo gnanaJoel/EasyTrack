161514a9067e0d1461db1537ba54280cf2338c84
adityac14 <adityachatto14@gmail.com>
2021-03-27 19:37:35 -0400

    Created the Search by Product Name UI

---
+++ app/src/main/java/com/tri_devs/easytrack/barcodescanning/SearchProductName.kt
@@ -0,0 +1,12 @@
+package com.tri_devs.easytrack.barcodescanning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tri_devs.easytrack.R
+
+class SearchProductName : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_product_name)
+    }
+}
