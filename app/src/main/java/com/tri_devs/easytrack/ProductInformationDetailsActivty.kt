0de2d0c67b7fa0b21438c551d2935179e86e459e
Harvey Ho <harvey_1997@hotmail.ca>
2021-03-27 19:09:48 -0400

    added ProductInformationDetailsActivty.kt

---
+++ app/src/main/java/com/tri_devs/easytrack/ProductInformationDetailsActivty.kt
@@ -0,0 +1,11 @@
+package com.tri_devs.easytrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductInformationDetailsActivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_information_details_activty)
+    }
+}
