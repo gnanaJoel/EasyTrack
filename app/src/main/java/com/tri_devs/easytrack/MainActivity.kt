8e167a1792c47f412daf5ed02162589bfe3d0a06
joelgnanasekaram <gnanasej@sheridancollege.ca>
2021-03-27 17:45:42 -0400

    Initial commit of the EasyTrack Barcode Mobile Application Project

---
+++ app/src/main/java/com/tri_devs/easytrack/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tri_devs.easytrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
