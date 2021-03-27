ed6d1b7b1f77fb1b449f86d663111e664ebfccbf
adityac14 <adityachatto14@gmail.com>
2021-03-27 19:16:53 -0400

    Created the Sales Associate Home Screen UI and id's for all the widgets in both the Login/Sales Associate Screens.

---
+++ app/src/main/java/com/tri_devs/easytrack/barcodescanning/SalesAssociateHome.kt
@@ -0,0 +1,12 @@
+package com.tri_devs.easytrack.barcodescanning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tri_devs.easytrack.R
+
+class SalesAssociateHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sales_associate_home)
+    }
+}
