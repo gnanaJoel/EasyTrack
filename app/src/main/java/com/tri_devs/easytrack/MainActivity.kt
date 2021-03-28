f180eb0ce232a5448752afa7c13a41fd5c873a64
joelgnanasekaram <gnanasej@sheridancollege.ca>
2021-03-27 21:01:36 -0400

    Created the Warehouse Manager Home Activity Kotlin Class in the Barcode Generation folder as well as Warehouse Manager Home Activity Layout XML File in the layout folder in the resources folder, and initially designed the User Interface for the Warehouse Manager Home Activity Layout in the Warehouse Manager Home Activity Layout XML File in the layout folder of the of the resources folder of the EasyTrack Barcode Mobile Application.

--- app/src/main/java/com/tri_devs/easytrack/MainActivity.kt
+++ app/src/main/java/com/tri_devs/easytrack/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.tri_devs.easytrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.tri_devs.easytrack.R
+import com.tri_devs.easytrack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, WarehouseManagerHomeActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
