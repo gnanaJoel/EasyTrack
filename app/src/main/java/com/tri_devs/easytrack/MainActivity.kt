4a564f402ecd63329afab5a6418185209e978245
Harvey Ho <harvey_1997@hotmail.ca>
2021-03-27 20:23:56 -0400

    set up test intents to move between activities

--- app/src/main/java/com/tri_devs/easytrack/MainActivity.kt
+++ app/src/main/java/com/tri_devs/easytrack/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.tri_devs.easytrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.tri_devs.easytrack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, DepartmentManagerHomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
