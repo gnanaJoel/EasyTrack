e43f76aa6638ce194969257bdcaa274f2a40229a
adityac14 <adityachatto14@gmail.com>
2021-03-27 22:44:10 -0400

    Created the UI for the Warehouse Manager Summary Screen to confirm valid information was provided for the new product

--- app/src/main/java/com/tri_devs/easytrack/barcodegeneration/WarehouseManagerConfirmInsert.kt
+++ app/src/main/java/com/tri_devs/easytrack/barcodegeneration/WarehouseManagerConfirmInsert.kt
@@ -1,12 +1,38 @@
 package com.tri_devs.easytrack.barcodegeneration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.tri_devs.easytrack.R
+import com.tri_devs.easytrack.databinding.ActivityMainBinding
+import com.tri_devs.easytrack.databinding.ActivityWarehouseManagerConfirmInsertBinding
 
 class WarehouseManagerConfirmInsert : AppCompatActivity() {
+    lateinit var binding: ActivityWarehouseManagerConfirmInsertBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_warehouse_manager_confirm_insert)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_warehouse_manager_confirm_insert)
+
+        binding.btnYes.setOnClickListener { yes() }
+
+        binding.btnNo.setOnClickListener { no() }
+
+    }
+
+    fun yes(){
+        Toast.makeText(this, "Product Info Successfully saved!", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, WarehouseManagerHomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun no(){
+        val intent = Intent(this, WarehouseManagerInsertNewProductInfoActivity::class.java)
+        startActivity(intent)
+
     }
 }
