d74780e895aff1f52a3eb37566efba766b1ef8cd
Harvey Ho <harvey_1997@hotmail.ca>
2021-03-27 22:03:57 -0400

    fixed barcode scanning

--- app/src/main/java/com/tri_devs/easytrack/barcodescanning/BarcodeScanning.kt
+++ app/src/main/java/com/tri_devs/easytrack/barcodescanning/BarcodeScanning.kt
@@ -1,13 +1,23 @@
 package com.tri_devs.easytrack.barcodescanning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.tri_devs.easytrack.R
+import com.tri_devs.easytrack.databinding.ActivityBarcodeScanningBinding
 
 class BarcodeScanning : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBarcodeScanningBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_barcode_scanning)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_barcode_scanning)
+        binding.btnScanThis.setOnClickListener {
+            val intent = Intent(this, ProductInfoSearch::class.java)
+            startActivity(intent)
+        }
 
     }
 }
