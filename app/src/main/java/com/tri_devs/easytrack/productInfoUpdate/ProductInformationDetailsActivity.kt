72aec5294774361218eb4885837a05dd4adf3c0b
Harvey Ho <harvey_1997@hotmail.ca>
2021-03-27 21:41:21 -0400

    dialog

--- app/src/main/java/com/tri_devs/easytrack/productInfoUpdate/ProductInformationDetailsActivity.kt
+++ app/src/main/java/com/tri_devs/easytrack/productInfoUpdate/ProductInformationDetailsActivity.kt
@@ -1,9 +1,12 @@
 package com.tri_devs.easytrack.productInfoUpdate
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.tri_devs.easytrack.R
 import com.tri_devs.easytrack.databinding.ActivityProductInformationDetailsActivtyBinding
@@ -38,11 +41,36 @@ class ProductInformationDetailsActivity : AppCompatActivity() {
     }
 
     private fun submitUpdate() {
-        TODO("Not yet implemented")
+        confirmationDialog()
     }
 
     private fun backToMenu(){
         val intent = Intent(this, DepartmentManagerHomeActivity::class.java)
         startActivity(intent)
     }
+
+    private fun confirmationDialog(){
+        val dialogBuilder = AlertDialog.Builder(this)
+
+        // set message of alert dialog
+        dialogBuilder.setMessage("Are you sure you want to update?")
+            // if the dialog is cancelable
+            .setCancelable(false)
+            // positive button text and action
+            .setPositiveButton("Confirm") { _, _ ->
+                finish()
+            }
+            // negative button text and action
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.cancel()
+            }
+
+        // create dialog box
+        val alert = dialogBuilder.create()
+        // set title for alert dialog box
+        alert.setTitle("Confirm Update")
+        // show alert dialog
+        alert.show()
+    }
+
 }
