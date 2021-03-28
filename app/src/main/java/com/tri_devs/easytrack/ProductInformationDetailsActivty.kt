fba32fbb56b31b81ab8d44e760e53404745935c8
Harvey Ho <harvey_1997@hotmail.ca>
2021-03-27 20:16:27 -0400

    added binding to ProductInformationDetailsActivty.kt

--- app/src/main/java/com/tri_devs/easytrack/ProductInformationDetailsActivty.kt
+++ app/src/main/java/com/tri_devs/easytrack/ProductInformationDetailsActivty.kt
@@ -2,10 +2,26 @@ package com.tri_devs.easytrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.tri_devs.easytrack.databinding.ActivityProductInformationDetailsActivtyBinding
 
 class ProductInformationDetailsActivty : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProductInformationDetailsActivtyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_information_details_activty)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product_information_details_activty)
+
+        binding.btnSearchByName.setOnClickListener { searchProductByName() }
+        binding.btnSubmit.setOnClickListener {  submitUpdate() }
+    }
+
+    private fun searchProductByName() {
+        TODO("Not yet implemented")
+    }
+
+    private fun submitUpdate() {
+        TODO("Not yet implemented")
     }
 }
