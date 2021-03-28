edf9d13342f7f22cbc71192cf0a547de6f9333f0
adityac14 <adityachatto14@gmail.com>
2021-03-27 22:00:30 -0400

    Created the UI for the Insert New Product Page for the Warehouse Manager

---
+++ app/src/main/java/com/tri_devs/easytrack/barcodegeneration/WarehouseManagerConfirmInsert.kt
@@ -0,0 +1,12 @@
+package com.tri_devs.easytrack.barcodegeneration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tri_devs.easytrack.R
+
+class WarehouseManagerConfirmInsert : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_warehouse_manager_confirm_insert)
+    }
+}
