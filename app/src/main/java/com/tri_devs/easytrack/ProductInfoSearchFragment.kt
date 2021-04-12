e33bd4b1b60553959bf87bd04d594ecde52ad8bf
Harvey Ho <harvey_1997@hotmail.ca>
2021-04-12 13:08:05 -0400

    added ProductInfoSearchFragment.kt

--- app/src/main/java/com/tri_devs/easytrack/ProductInfoSearchFragment.kt
+++ app/src/main/java/com/tri_devs/easytrack/ProductInfoSearchFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ProductInfoFragment.newInstance] factory method to
+ * Use the [ProductInfoSearchFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ProductInfoFragment : Fragment() {
+class ProductInfoSearchFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class ProductInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_info, container, false)
+        return inflater.inflate(R.layout.fragment_product_info_search, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class ProductInfoFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ProductInfoFragment.
+         * @return A new instance of fragment ProductInfoSearchFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ProductInfoFragment().apply {
+            ProductInfoSearchFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
