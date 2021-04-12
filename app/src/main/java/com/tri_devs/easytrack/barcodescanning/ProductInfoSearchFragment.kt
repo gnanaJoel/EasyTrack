2e4e09713d9c40ca04a9376913b4f98db1fcde20
Harvey Ho <harvey_1997@hotmail.ca>
2021-04-12 13:13:35 -0400

    moved ProductInfoSearchFragment to barcodescanning folder

--- app/src/main/java/com/tri_devs/easytrack/barcodescanning/ProductInfoSearchFragment.kt
+++ app/src/main/java/com/tri_devs/easytrack/barcodescanning/ProductInfoSearchFragment.kt
@@ -1,10 +1,11 @@
-package com.tri_devs.easytrack
+package com.tri_devs.easytrack.barcodescanning
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.tri_devs.easytrack.R
 
 class ProductInfoSearchFragment : Fragment() {
 
