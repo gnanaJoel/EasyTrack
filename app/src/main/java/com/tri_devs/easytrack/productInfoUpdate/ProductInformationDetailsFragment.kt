24f962b8340da5686f1a05f28435fcd68366ace4
Harvey Ho <harvey_1997@hotmail.ca>
2021-04-13 21:56:34 -0400

    testing firestore

--- app/src/main/java/com/tri_devs/easytrack/productInfoUpdate/ProductInformationDetailsFragment.kt
+++ app/src/main/java/com/tri_devs/easytrack/productInfoUpdate/ProductInformationDetailsFragment.kt
@@ -3,6 +3,7 @@ package com.tri_devs.easytrack.productInfoUpdate
 import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,12 +11,17 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.tri_devs.easytrack.R
 import com.tri_devs.easytrack.databinding.FragmentProductInformationDetailsBinding
 
 class ProductInformationDetailsFragment : Fragment() {
     lateinit var binding: FragmentProductInformationDetailsBinding
     val args: ProductInformationDetailsFragmentArgs by navArgs()
+    val db = Firebase.firestore
+    val productsRef = db.collection("products")
+    val TAG = "Prod Info Details"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,6 +42,17 @@ class ProductInformationDetailsFragment : Fragment() {
 
         binding.btnSearchByName.setOnClickListener { searchProductByName() }
         binding.btnSubmit.setOnClickListener {  submitUpdate() }
+
+        //db part
+        val query = productsRef.whereEqualTo("upcNumber","91827364598")
+        query.get().addOnSuccessListener { documents ->
+            for (document in documents) {
+                Log.d(TAG, "${document.id} => ${document.data}")
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
 
         return binding.root
     }
