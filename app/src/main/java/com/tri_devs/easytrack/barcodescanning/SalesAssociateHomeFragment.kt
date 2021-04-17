7611b8eeafb494dc1c4d4246f7a22511013b8ec4
adityac14 <adityachatto14@gmail.com>
2021-04-17 15:26:31 -0400

    Scanning Demo 2 attempt

--- app/src/main/java/com/tri_devs/easytrack/barcodescanning/SalesAssociateHomeFragment.kt
+++ app/src/main/java/com/tri_devs/easytrack/barcodescanning/SalesAssociateHomeFragment.kt
@@ -1,6 +1,4 @@
 package com.tri_devs.easytrack.barcodescanning
-
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -18,14 +16,13 @@ import com.tri_devs.easytrack.R
 import com.tri_devs.easytrack.databinding.FragmentSalesAssociateHomeBinding
 import com.tri_devs.easytrack.entities.Product
 
-
 class SalesAssociateHomeFragment : Fragment() {
     lateinit var binding: FragmentSalesAssociateHomeBinding
     var scannedResult: String = ""
-    val TAG = "SCAN"
+    val TAG = "BARSCAN"
     val db = Firebase.firestore
     val productsRef = db.collection("products")
-    lateinit var productInfo: Product
+    //lateinit var productInfo: Product
 
 
     override fun onCreateView(
@@ -46,16 +43,117 @@ class SalesAssociateHomeFragment : Fragment() {
 
         return binding.root
 
-        Log.d(TAG, "In the oncreate!")
+//        Log.d(TAG, "In the oncreate!")
     }
+
+    fun search() {
+        findNavController().navigate(R.id.action_salesAssociateHomeFragment_to_seachProductNameFragment)
+    }
+
+    fun view() {
+        findNavController().navigate(R.id.action_salesAssociateHomeFragment_to_storeInventoryActivity)
+    }
+
 
     fun scan() {
         run {
             IntentIntegrator.forSupportFragment(this).initiateScan()
-            Log.d(TAG, scannedResult)
-
+//            Log.d(TAG, scannedResult)
         }
+        getsingleProduct()
+//        Toast.makeText(activity, scannedResult, Toast.LENGTH_SHORT).show()
+
+
+//        if (scannedResult.isNotEmpty()){
+//            //getProduct()
+//        }
+//        else{
+//            Toast.makeText(activity, "Invalid UPC Number", Toast.LENGTH_SHORT).show()
+//        }
     }
+
+    fun getsingleProduct(){
+        productsRef
+            .whereEqualTo("upcNumber", scannedResult)
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    //Toast.makeText(activity, document.toString(), Toast.LENGTH_SHORT).show()
+                    //document.get("name").toString()
+                    binding.txtDoc.text = document.get("name").toString()
+
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "Error getting documents: ", exception)
+            }
+
+    }
+
+//    fun getsingleProduct(){
+//       // scannedResult.val
+//        val docRef = productsRef.document(scannedResult)
+//        docRef.get()
+//            .addOnSuccessListener { document ->
+//                if (document != null) {
+//                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+//                    Toast.makeText(activity, "UPC number exists", Toast.LENGTH_SHORT).show()
+//                } else {
+//                    Log.d(TAG, "No such document")
+//                    Toast.makeText(activity, "UPC number doesn't exist", Toast.LENGTH_SHORT).show()
+//
+//                }
+//            }
+//            .addOnFailureListener { exception ->
+//                Log.d(TAG, "Unknown error ", exception)
+//                Toast.makeText(activity, "Unknown error", Toast.LENGTH_SHORT).show()
+//
+//            }
+//
+//    }
+
+    fun getProduct(){
+        val query = productsRef.whereEqualTo("upcNumber", scannedResult)
+        query.get().addOnSuccessListener { documents ->
+            for (document in documents) {
+                if (document != null) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    val productInfo = Product(
+                        document.get("name").toString(),
+                        document.get("description").toString(),
+                        document.get("quantity").toString().toInt(),
+                        document.get("upcNumber").toString().toLong(),
+                        document.get("retailPrice").toString(),
+                        document.get("salesPrice").toString(),
+                        document.get("startSalesDate").toString(),
+                        document.get("endSalesDate").toString()
+                    )
+
+                    val action = SalesAssociateHomeFragmentDirections.goToProductInfoSearch(productInfo)
+                    findNavController().navigate(action)
+
+
+                } else {
+                    Toast.makeText(activity, "Invalid UPC Number", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+                Toast.makeText(activity, "Can't connect to the db", Toast.LENGTH_SHORT)
+                    .show()
+
+            }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState?.putString("scannedResult", scannedResult)
+        super.onSaveInstanceState(outState)
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         var result: IntentResult? =
@@ -66,87 +164,13 @@ class SalesAssociateHomeFragment : Fragment() {
             if (result.contents != null) {
                 scannedResult = result.contents
                 binding.txtUPC.text = scannedResult
-                Log.d(TAG, scannedResult)
-                Toast.makeText(activity, scannedResult, Toast.LENGTH_LONG).show()
-
-
-                val query = productsRef.whereEqualTo("upcNumber", scannedResult)
-                query.get().addOnSuccessListener { documents ->
-                    for (document in documents) {
-                        if (document != null) {
-                            Log.d(TAG, "${document.id} => ${document.data}")
-                            productInfo = Product(
-                                document.get("name").toString(),
-                                document.get("description").toString(),
-                                document.get("quantity").toString().toInt(),
-                                document.get("upcNumber").toString().toLong(),
-                                document.get("retailPrice").toString(),
-                                document.get("salesPrice").toString(),
-                                document.get("startSalesDate").toString(),
-                                document.get("endSalesDate").toString()
-                            )
-
-
-                        } else {
-                            Toast.makeText(activity, "Invalid UPC Number", Toast.LENGTH_SHORT)
-                                .show()
-                        }
-                    }
-                    if(productInfo != null){
-                        val action = SalesAssociateHomeFragmentDirections.goToProductInfoSearch(productInfo)
-                        findNavController().navigate(action)
-
-                    }
-                    else{
-                        Toast.makeText(activity, "Product doesn't exist", Toast.LENGTH_SHORT)
-                            .show()
-
-                    }
-
-
-                }
-                    .addOnFailureListener { exception ->
-                        Log.w(TAG, "Error getting documents: ", exception)
-                        Toast.makeText(activity, "Can't connect to the db", Toast.LENGTH_SHORT)
-                            .show()
-
-                    }
-
-
-            } else {
-                binding.txtUPC.text = "scan failed"
-                Log.d(TAG, scannedResult)
-                Toast.makeText(activity, scannedResult, Toast.LENGTH_LONG).show()
-
+//                Log.d(TAG, scannedResult)
+                //Toast.makeText(activity, scannedResult, Toast.LENGTH_LONG).show()
 
             }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
-            Log.d(TAG, scannedResult)
-
         }
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        savedInstanceState?.let {
-            scannedResult = it.getString("scannedResult")!!
-            binding.txtUPC.text = scannedResult
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState?.putString("scannedResult", scannedResult)
-        super.onSaveInstanceState(outState)
-    }
 
 
-    fun search() {
-        findNavController().navigate(R.id.action_salesAssociateHomeFragment_to_seachProductNameFragment)
-    }
-
-    fun view() {
-        findNavController().navigate(R.id.action_salesAssociateHomeFragment_to_storeInventoryActivity)
     }
 
 }
